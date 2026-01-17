7b826a2239942c14c157f4cbb7a06ee10b3438ad
matheus-dev-fs <matheusmks10@gmail.com>
2026-01-17 12:31:46 -0300

    refactor: separate views into partials and static pages
    - split header and footer into mustache partials
    - create static page templates for each route
    - controllers render static content for now; dynamic data to be added later

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,7 +1,7 @@
 import type { Request, Response } from 'express';
 
 export const home = (req: Request, res: Response): void => {
-    res.send('Welcome to the Home Page');
+    res.render('pages/page');   
 };
 
 export const dogs = (req: Request, res: Response): void => {
