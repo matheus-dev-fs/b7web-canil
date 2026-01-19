be0ca313d108950bc923fafe8629e6d579ed2407
matheus-dev-fs <matheusmks10@gmail.com>
2026-01-19 17:03:23 -0300

    feat: implement pet search functionality
    - add search controller to filter pets by name
    - display search results and term in the template
    - update header and page templates to support dynamic search

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,17 @@
 import type { Request, Response } from 'express';
+import type { MenuOptions } from '../interfaces/menuOptions.interface.js';
+import { createMenuOptions } from '../helpers/createMenuObject.helper.js';
+import type { PetData } from '../interfaces/petData.interface.js';
+import { Pet } from '../models/pet.model.js';
 
 export const search = (req: Request, res: Response): void => {
-    res.send('Welcome to the Search Page');
+    const query: string = req.query.q as string;
+    const list: PetData[] = Pet.getByName(query);
+    const menu: MenuOptions = createMenuOptions('');
+
+    res.render('pages/page', {
+        menu,
+        list,
+        searchTerm: query
+    });
 };
